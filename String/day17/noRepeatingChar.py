1761fa6dbf6de65bb12d66e69a7af0628a20318c
harshal dafe <harshaldafe@outlook.com>
2026-07-12 14:47:08 +0530

    day17 12/07/2026

---
+++ String/day17/noRepeatingChar.py
@@ -0,0 +1,24 @@
+# def nonRepeatingChar(self,s):
+#     #code here
+#     list_of_s = list(s)
+        
+#     for el in list_of_s:
+#         if list_of_s.count(el) == 1:
+#             return el
+        
+#     return '$'
+# 1110/1125
+# time limit exceeds
+
+# def nonRepeatingChar(self,s):
+#         #code here
+#         # list_of_s = list(s)
+        
+#         for idx in range(len(s)):
+#             if s.count(s[idx]) == 1:
+#                 return s[idx]
+                
+#         return '$'
+# # time limit exceeds
+# # 1120/1125
+
